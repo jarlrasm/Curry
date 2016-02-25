36238d41a71a34cbbff4104af09de72622baff7c
Jarl Rasmussen <jarl@okb.no>
2016-02-25 15:36:18 +0100

    Added nuspec

--- Curry/Properties/AssemblyInfo.cs
+++ Curry/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Curry")]
+[assembly: AssemblyTitle("Functional")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Curry")]
+[assembly: AssemblyCompany("Jarl Sveinung Rasmussen")]
+[assembly: AssemblyProduct("Functional")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
